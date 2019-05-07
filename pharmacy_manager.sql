4df2d4fe1c8c6c1349994ba6deb475f83b221c68
Sibtain Ali <allroundexperts@gmail.com>
2019-05-07 21:43:06 +0500

    Version 4.

--- pharmacy_manager.sql
+++ pharmacy_manager.sql
@@ -7,11 +7,9 @@
 -- Server version: 10.1.39-MariaDB
 -- PHP Version: 7.3.5
 
-SET AUTOCOMMIT
-= 0;
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
-SET time_zone
-= "+00:00";
+SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -29,24 +27,17 @@ SET time_zone
 -- Table structure for table `branch`
 --
 
-CREATE TABLE `branch`
-(
-  `BranchID` int
-(11) NOT NULL,
-  `Location` varchar
-(255) DEFAULT NULL,
-  `City` varchar
-(255) DEFAULT NULL
+CREATE TABLE `branch` (
+  `BranchID` int(11) NOT NULL,
+  `Location` varchar(255) DEFAULT NULL,
+  `City` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `branch`
 --
 
-INSERT INTO `branch` (`
-BranchID`,
-`Location
-`, `City`) VALUES
+INSERT INTO `branch` (`BranchID`, `Location`, `City`) VALUES
 (1, 'Faisal Town', 'Lahore'),
 (4, 'Zero Point', 'Islamabad');
 
@@ -56,24 +47,17 @@ BranchID`,
 -- Table structure for table `customer`
 --
 
-CREATE TABLE `customer`
-(
-  `CustomerID` int
-(11) NOT NULL,
-  `Name` varchar
-(255) DEFAULT NULL,
-  `PrescriptionID` varchar
-(255) DEFAULT NULL
+CREATE TABLE `customer` (
+  `CustomerID` int(11) NOT NULL,
+  `Name` varchar(255) DEFAULT NULL,
+  `PrescriptionID` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `customer`
 --
 
-INSERT INTO `customer` (`
-CustomerID`,
-`Name
-`, `PrescriptionID`) VALUES
+INSERT INTO `customer` (`CustomerID`, `Name`, `PrescriptionID`) VALUES
 (9050, 'Junaid Ali', '4546'),
 (9064, 'Pervez Tariq', '1932'),
 (9066, 'Armeena Rana', '1549'),
@@ -89,32 +73,21 @@ CustomerID`,
 -- Table structure for table `employee`
 --
 
-CREATE TABLE `employee`
-(
-  `EmployeeID` int
-(11) NOT NULL,
-  `Name` varchar
-(255) DEFAULT NULL,
-  `Email` varchar
-(255) NOT NULL,
-  `Password` varchar
-(255) NOT NULL,
-  `Age` int
-(11) DEFAULT NULL,
-  `Gender` varchar
-(255) DEFAULT NULL,
-  `BranchID` int
-(11) DEFAULT NULL
+CREATE TABLE `employee` (
+  `EmployeeID` int(11) NOT NULL,
+  `Name` varchar(255) DEFAULT NULL,
+  `Email` varchar(255) NOT NULL,
+  `Password` varchar(255) NOT NULL,
+  `Age` int(11) DEFAULT NULL,
+  `Gender` varchar(255) DEFAULT NULL,
+  `BranchID` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `employee`
 --
 
-INSERT INTO `employee` (`
-EmployeeID`,
-`Name
-`, `Email`, `Password`, `Age`, `Gender`, `BranchID`) VALUES
+INSERT INTO `employee` (`EmployeeID`, `Name`, `Email`, `Password`, `Age`, `Gender`, `BranchID`) VALUES
 (1217, 'Jon Snow', 'jonsnow@gmail.com', 'test1234', 26, 'male', 1);
 
 -- --------------------------------------------------------
@@ -123,14 +96,10 @@ EmployeeID`,
 -- Table structure for table `medicineorders`
 --
 
-CREATE TABLE `medicineorders`
-(
-  `OrderID` int
-(11) DEFAULT NULL,
-  `MedNo` int
-(11) DEFAULT NULL,
-  `Quantity` int
-(11) DEFAULT NULL
+CREATE TABLE `medicineorders` (
+  `OrderID` int(11) DEFAULT NULL,
+  `MedNo` int(11) DEFAULT NULL,
+  `Quantity` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -139,30 +108,20 @@ CREATE TABLE `medicineorders`
 -- Table structure for table `medicines`
 --
 
-CREATE TABLE `medicines`
-(
-  `MedNo` int
-(11) NOT NULL,
-  `Name` varchar
-(255) DEFAULT NULL,
-  `MfgDate` varchar
-(255) DEFAULT NULL,
-  `ExpDate` varchar
-(255) DEFAULT NULL,
-  `Price` int
-(11) DEFAULT NULL,
-  `Quantity` int
-(11) DEFAULT NULL
+CREATE TABLE `medicines` (
+  `MedNo` int(11) NOT NULL,
+  `Name` varchar(255) DEFAULT NULL,
+  `MfgDate` varchar(255) DEFAULT NULL,
+  `ExpDate` varchar(255) DEFAULT NULL,
+  `Price` int(11) DEFAULT NULL,
+  `Quantity` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `medicines`
 --
 
-INSERT INTO `medicines` (`
-MedNo`,
-`Name
-`, `MfgDate`, `ExpDate`, `Price`, `Quantity`) VALUES
+INSERT INTO `medicines` (`MedNo`, `Name`, `MfgDate`, `ExpDate`, `Price`, `Quantity`) VALUES
 (2, 'Zyloric', '2/2/2019', '12/12/2019', 250, 2);
 
 -- --------------------------------------------------------
@@ -171,18 +130,12 @@ MedNo`,
 -- Table structure for table `orders`
 --
 
-CREATE TABLE `orders`
-(
-  `OrderID` int
-(11) NOT NULL,
-  `CustomerID` int
-(11) DEFAULT NULL,
-  `BranchID` int
-(11) DEFAULT NULL,
-  `Payment` int
-(11) DEFAULT NULL,
-  `Date` varchar
-(255) DEFAULT NULL
+CREATE TABLE `orders` (
+  `OrderID` int(11) NOT NULL,
+  `CustomerID` int(11) DEFAULT NULL,
+  `BranchID` int(11) DEFAULT NULL,
+  `Payment` int(11) DEFAULT NULL,
+  `Date` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -193,51 +146,41 @@ CREATE TABLE `orders`
 -- Indexes for table `branch`
 --
 ALTER TABLE `branch`
-ADD PRIMARY KEY
-(`BranchID`);
+  ADD PRIMARY KEY (`BranchID`);
 
 --
 -- Indexes for table `customer`
 --
 ALTER TABLE `customer`
-ADD PRIMARY KEY
-(`CustomerID`);
+  ADD PRIMARY KEY (`CustomerID`);
 
 --
 -- Indexes for table `employee`
 --
 ALTER TABLE `employee`
-ADD PRIMARY KEY
-(`EmployeeID`),
-ADD KEY `BranchID`
-(`BranchID`);
+  ADD PRIMARY KEY (`EmployeeID`),
+  ADD KEY `BranchID` (`BranchID`);
 
 --
 -- Indexes for table `medicineorders`
 --
 ALTER TABLE `medicineorders`
-ADD KEY `OrderID`
-(`OrderID`),
-ADD KEY `MedNo`
-(`MedNo`);
+  ADD KEY `OrderID` (`OrderID`),
+  ADD KEY `MedNo` (`MedNo`);
 
 --
 -- Indexes for table `medicines`
 --
 ALTER TABLE `medicines`
-ADD PRIMARY KEY
-(`MedNo`);
+  ADD PRIMARY KEY (`MedNo`);
 
 --
 -- Indexes for table `orders`
 --
 ALTER TABLE `orders`
-ADD PRIMARY KEY
-(`OrderID`),
-ADD KEY `CustomerID`
-(`CustomerID`),
-ADD KEY `BranchID`
-(`BranchID`);
+  ADD PRIMARY KEY (`OrderID`),
+  ADD KEY `CustomerID` (`CustomerID`),
+  ADD KEY `BranchID` (`BranchID`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -247,36 +190,31 @@ ADD KEY `BranchID`
 -- AUTO_INCREMENT for table `branch`
 --
 ALTER TABLE `branch`
-  MODIFY `BranchID` int
-(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `BranchID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `customer`
 --
 ALTER TABLE `customer`
-  MODIFY `CustomerID` int
-(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9078;
+  MODIFY `CustomerID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9078;
 
 --
 -- AUTO_INCREMENT for table `employee`
 --
 ALTER TABLE `employee`
-  MODIFY `EmployeeID` int
-(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1218;
+  MODIFY `EmployeeID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1218;
 
 --
 -- AUTO_INCREMENT for table `medicines`
 --
 ALTER TABLE `medicines`
-  MODIFY `MedNo` int
-(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `MedNo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `OrderID` int
-(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `OrderID` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
@@ -286,31 +224,21 @@ ALTER TABLE `orders`
 -- Constraints for table `employee`
 --
 ALTER TABLE `employee`
-ADD CONSTRAINT `Employee_ibfk_1` FOREIGN KEY
-(`BranchID`) REFERENCES `branch`
-(`BranchID`);
+  ADD CONSTRAINT `Employee_ibfk_1` FOREIGN KEY (`BranchID`) REFERENCES `branch` (`BranchID`);
 
 --
 -- Constraints for table `medicineorders`
 --
 ALTER TABLE `medicineorders`
-ADD CONSTRAINT `MedicineOrders_ibfk_1` FOREIGN KEY
-(`OrderID`) REFERENCES `orders`
-(`OrderID`),
-ADD CONSTRAINT `MedicineOrders_ibfk_2` FOREIGN KEY
-(`MedNo`) REFERENCES `medicines`
-(`MedNo`);
+  ADD CONSTRAINT `MedicineOrders_ibfk_1` FOREIGN KEY (`OrderID`) REFERENCES `orders` (`OrderID`),
+  ADD CONSTRAINT `MedicineOrders_ibfk_2` FOREIGN KEY (`MedNo`) REFERENCES `medicines` (`MedNo`);
 
 --
 -- Constraints for table `orders`
 --
 ALTER TABLE `orders`
-ADD CONSTRAINT `Orders_ibfk_1` FOREIGN KEY
-(`CustomerID`) REFERENCES `customer`
-(`CustomerID`),
-ADD CONSTRAINT `Orders_ibfk_2` FOREIGN KEY
-(`BranchID`) REFERENCES `branch`
-(`BranchID`);
+  ADD CONSTRAINT `Orders_ibfk_1` FOREIGN KEY (`CustomerID`) REFERENCES `customer` (`CustomerID`),
+  ADD CONSTRAINT `Orders_ibfk_2` FOREIGN KEY (`BranchID`) REFERENCES `branch` (`BranchID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
